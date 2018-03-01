c2fcb3992783162221f44f1bc5a3e50945f4e5db
Ekaterina <Ekaterina Korobova>
2018-03-01 21:20:39 +0300

    Другая система координат + клеточки по краям

--- src/Hive.hs
+++ src/Hive.hs
@@ -7,7 +7,7 @@ runHive :: IO ()
 runHive = do
   play display bgColor fps initGame drawGame handleGame updateGame
   where
-    display = InWindow "Улей" (screenWidth, screenHeight) (0, 0)
+    display = InWindow "Hive" (screenWidth, screenHeight) (0, 0)
     bgColor = white   -- цвет фона
     fps     = 0      -- кол-во кадров в секунду
 
@@ -29,7 +29,7 @@ data Cell = Cell
 -- | Игрок				
 data Player = Black | Beige
 
--- | Игровое поле
+-- | Поле
 type Board = [Cell]
 
 -- | Состояние игры
@@ -44,33 +44,61 @@ data Game = Game
 -- Первый игрок ходит бежевыми.
 initGame :: Game
 initGame = Game
-  { gameBoard  = createCells 0 11
+  { gameBoard  = createCells (-11) (-11) ++ createPieces
   , gamePlayer = Beige
   , gameWinner = Nothing
   }
 
--- | Создаем список из всех клеток поля
+-- | Создаем список из клеток игрового поля
 createCells :: Int -> Int -> Board
 createCells xx yy
-  | xx > 22 = []
-  | xx + yy >= 55 = this : createCells (xx + 1) (xx - 10)
-  | yy - xx >= 33 = this : createCells (xx + 1) (10 - xx)
+  | xx > n = []
+  | xx + yy >= 2 * n = this : createCells (xx + 1) (xx + 1 - 2 * n)
+  | yy - xx >= 2 * n = this : createCells (xx + 1) (- 2 * n - xx - 1)
   | otherwise = this : createCells xx (yy + 2)
   where
     this = Cell { x = xx, y = yy, insects = []}
+    n = numberOfPieces
 
+-- | Создаем список из клеток, в которых вначале находятся фишки
+createPieces :: Board
+createPieces =
+  [ Cell{x = -xx, y = -10, insects = [(Beige, Queen)]}
+  , Cell{x = -xx, y = -8, insects = [(Beige, Spider)]}
+  , Cell{x = -xx, y = -6, insects = [(Beige, Spider)]}
+  , Cell{x = -xx, y = -4, insects = [(Beige, Beetle)]}
+  , Cell{x = -xx, y = -2, insects = [(Beige, Beetle)]}
+  , Cell{x = -xx, y = 0, insects = [(Beige, Hopper)]}
+  , Cell{x = -xx, y = 2, insects = [(Beige, Hopper)]}
+  , Cell{x = -xx, y = 4, insects = [(Beige, Hopper)]}
+  , Cell{x = -xx, y = 6, insects = [(Beige, Ant)]}
+  , Cell{x = -xx, y = 8, insects = [(Beige, Ant)]}
+  , Cell{x = -xx, y = 10, insects = [(Beige, Ant)]}
+
+  , Cell{x = xx, y = -10, insects = [(Black, Queen)]}
+  , Cell{x = xx, y = -8, insects = [(Black, Spider)]}
+  , Cell{x = xx, y = -6, insects = [(Black, Spider)]}
+  , Cell{x = xx, y = -4, insects = [(Black, Beetle)]}
+  , Cell{x = xx, y = -2, insects = [(Black, Beetle)]}
+  , Cell{x = xx, y = 0, insects = [(Black, Hopper)]}
+  , Cell{x = xx, y = 2, insects = [(Black, Hopper)]}
+  , Cell{x = xx, y = 4, insects = [(Black, Hopper)]}
+  , Cell{x = xx, y = 6, insects = [(Black, Ant)]}
+  , Cell{x = xx, y = 8, insects = [(Black, Ant)]}
+  , Cell{x = xx, y = 10, insects = [(Black, Ant)]}]
+  where
+    xx = cellDistance + numberOfPieces
+  
 -- =========================================
 -- Отрисовка игры
 -- =========================================
 
 -- | Отобразить игровое поле.
 drawGame :: Game -> Picture
-drawGame (Game{gameBoard = board}) = translate (-w) (-h) (scale cx cy (drawBoard board))
+drawGame (Game{gameBoard = board}) = (scale cx cy (drawBoard board))
   where
     cx = fromIntegral cellSizeX
     cy = fromIntegral cellSizeY
-    w = fromIntegral screenWidth  / 2
-    h = fromIntegral screenHeight / 2
 
 -- | Нарисовать поле
 drawBoard :: Board -> Picture
@@ -79,13 +107,13 @@ drawBoard board = pictures (map drawCell board)
 -- | Нарисовать клетку
 drawCell :: Cell -> Picture
 drawCell (Cell {x = xx, y = yy}) = line
-  [ (a + 1 / 3, b)
-  , (a + 1, b)
-  , (a + 4 / 3, b + 1)
-  , (a + 1, b + 2)
-  , (a + 1 / 3, b + 2)
-  , (a, b + 1)
-  , (a + 1 / 3, b)]
+  [ (a - 1 / 3, b - 1)
+  , (a + 1 / 3, b - 1)
+  , (a + 2 / 3, b)
+  , (a + 1 / 3, b + 1)
+  , (a - 1 / 3, b + 1)
+  , (a - 2 / 3, b)
+  , (a - 1 / 3, b - 1)]
   where 
     a = fromIntegral xx
     b = fromIntegral yy
@@ -113,11 +141,11 @@ numberOfPieces = 11
 
 -- | Ширина игрового поля в клетках.
 boardWidth :: Int
-boardWidth  = 2 * numberOfPieces + 2
+boardWidth  = 2 * (numberOfPieces + cellDistance) + 2
 
 -- | Высота игрового поля в клетках.
 boardHeight :: Int
-boardHeight = 4 * numberOfPieces + 2
+boardHeight = 4 * numberOfPieces + 3
 
 -- | Ширина одной клетки в пикселях.
 cellSizeX :: Int
@@ -134,3 +162,7 @@ screenWidth  = cellSizeX * boardWidth
 -- | Высота экрана в пикселях.
 screenHeight :: Int
 screenHeight = cellSizeY * boardHeight
+
+-- | Расстояние от игрового поля до фишек в начале игры, выраженное в клетках
+cellDistance :: Int
+cellDistance = 4
